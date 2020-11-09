503586f1468964e70068746ea16939d82e3acefc
Phamsy23061999 <55990150+Phamsy23061999@users.noreply.github.com>
2020-11-09 11:50:25 +0700

    code demo
    code

---
+++ com.ProjectJava/src/main/java/com/Entity/CategoryEntity.java
@@ -0,0 +1,39 @@
+package com.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Category")
+public class CategoryEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "categoryname")
+	private String categoryName;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	
+	
+}
